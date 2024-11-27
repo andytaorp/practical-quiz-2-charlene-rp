15bf9ad34e680c1167e48c96659491579269438e
charlene-rp <23024007@myrp.edu.sg>
2024-11-27 15:43:45 +0800

    first

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,28 @@
 import React from "react";
 
-/**
- * TODO: implement the Task component
- * @function Task
- * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
- */
-export default function Task() {}
+// /**
+//  * TODO: implement the Task component
+//  * @function Task
+//  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
+//  */
+
+function Task({ task, onToggleTask, onDeleteTask }) {
+  const taskStyle = task.completed ? { textDecoration: "line-through" } : {};
+
+  return (
+    <div>
+      <label style={taskStyle}>
+        <input
+          type="checkbox"
+          checked={task.completed} 
+          onChange={() => onToggleTask(task.id)} 
+        />
+        {task.description} x {task.quantity}
+      </label>
+      <button onClick={() => onDeleteTask(task.id)}>Delete</button>
+    </div>
+  );
+}
+
+export default Task;
+
