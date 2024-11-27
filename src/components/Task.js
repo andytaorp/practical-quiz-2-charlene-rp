ac4f6f870220bd5f62514e46070e3d4a1baaa1c7
charlene-rp <23024007@myrp.edu.sg>
2024-11-27 15:45:12 +0800

    updated

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,5 @@
 import React from "react";
 
-// /**
-//  * TODO: implement the Task component
-//  * @function Task
-//  * @returns {React.Component} A <li> containing the task description and a checkbox to toggle completion
-//  */
-
 function Task({ task, onToggleTask, onDeleteTask }) {
   const taskStyle = task.completed ? { textDecoration: "line-through" } : {};
 
